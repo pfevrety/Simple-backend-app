b17828b4b3bfa2d42d97e757d2becf6950624c01
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-07-18 11:07:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-const Thing = require('./schema/thing');
+const stuffRoutes = require('./routes/stuff.js');
 
 mongoose.connect('mongodb+srv://Fevret:a0MckJLgVN1Ffuzf@cluster.hat1x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -17,39 +17,11 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
 
 app.use(bodyParser.json())
 
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  })
-  thing.save().then(() => res.status(201).json({'message': 'Object added'})).catch(error => res.status(400).json({'error': error}));
-});
-
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
 
