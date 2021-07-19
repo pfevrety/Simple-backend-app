3b8c7f6a43d433ba8b012ea285505e393f4fee51
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-07-19 10:04:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 const app = express();
 
 const stuffRoutes = require('./routes/stuff.js');
+const userRoutes = require('./routes/auth.js');
 
-mongoose.connect('mongodb+srv://Fevret:a0MckJLgVN1Ffuzf@cluster.hat1x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.TOKEN}@cluster.hat1x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -22,6 +24,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json())
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
 
