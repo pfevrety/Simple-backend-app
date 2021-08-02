ecb8ce21a104952c2137398b8c76c8eba1ba81af
Fevret <efevret@gmail.Com>
2021-08-02 17:36:27 +0200

    Update stuff.js

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,4 +1,4 @@
-const Thing = require('../schema/thing');
+const Thing = require("../schema/thing");
 
 exports.createThing = (req, res, next) => {
   const thing = new Thing({
@@ -6,37 +6,34 @@ exports.createThing = (req, res, next) => {
     description: req.body.description,
     imageUrl: req.body.imageUrl,
     price: req.body.price,
-    userId: req.body.userId
+    userId: req.body.userId,
   });
-  thing.save().then(
-    () => {
+  thing
+    .save()
+    .then(() => {
       res.status(201).json({
-        message: 'Post saved successfully!'
+        message: "Post saved successfully!",
       });
-    }
-  ).catch(
-    (error) => {
+    })
+    .catch((error) => {
       res.status(400).json({
-        error: error
+        error: error,
       });
-    }
-  );
+    });
 };
 
 exports.getOneThing = (req, res, next) => {
   Thing.findOne({
-    _id: req.params.id
-  }).then(
-    (thing) => {
+    _id: req.params.id,
+  })
+    .then((thing) => {
       res.status(200).json(thing);
-    }
-  ).catch(
-    (error) => {
+    })
+    .catch((error) => {
       res.status(404).json({
-        error: error
+        error: error,
       });
-    }
-  );
+    });
 };
 
 exports.modifyThing = (req, res, next) => {
@@ -46,49 +43,43 @@ exports.modifyThing = (req, res, next) => {
     description: req.body.description,
     imageUrl: req.body.imageUrl,
     price: req.body.price,
-    userId: req.body.userId
+    userId: req.body.userId,
   });
-  Thing.updateOne({_id: req.params.id}, thing).then(
-    () => {
+  Thing.updateOne({ _id: req.params.id }, thing)
+    .then(() => {
       res.status(201).json({
-        message: 'Thing updated successfully!'
+        message: "Thing updated successfully!",
       });
-    }
-  ).catch(
-    (error) => {
+    })
+    .catch((error) => {
       res.status(400).json({
-        error: error
+        error: error,
       });
-    }
-  );
+    });
 };
 
 exports.deleteThing = (req, res, next) => {
-  Thing.deleteOne({_id: req.params.id}).then(
-    () => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => {
       res.status(200).json({
-        message: 'Deleted!'
+        message: "Deleted!",
       });
-    }
-  ).catch(
-    (error) => {
+    })
+    .catch((error) => {
       res.status(400).json({
-        error: error
+        error: error,
       });
-    }
-  );
+    });
 };
 
 exports.getAllStuff = (req, res, next) => {
-  Thing.find().then(
-    (things) => {
+  Thing.find()
+    .then((things) => {
       res.status(200).json(things);
-    }
-  ).catch(
-    (error) => {
+    })
+    .catch((error) => {
       res.status(400).json({
-        error: error
+        error: error,
       });
-    }
-  );
+    });
 };
