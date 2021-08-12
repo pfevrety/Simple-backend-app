d65837a1782292676e30038ddea9588dd37db7c2
Fevret <efevret@gmail.Com>
2021-08-12 15:43:23 +0200

    ✨ Update stuff controller

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,85 +1,76 @@
 const Thing = require("../schema/thing");
 
+const fs = require("fs");
+
 exports.createThing = (req, res, next) => {
-  const thing = new Thing({
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId,
-  });
-  thing
-    .save()
-    .then(() => {
-      res.status(201).json({
-        message: "Post saved successfully!",
-      });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
+    const thingObject = JSON.parse(req.body.thing);
+    delete thingObject._id;
+    const thing = new Thing({
+        ...thingObject,
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+            req.file.filename
+        }`,
     });
+    thing
+        .save()
+        .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+        .catch((error) => res.status(400).json({ error }));
 };
 
 exports.getOneThing = (req, res, next) => {
-  Thing.findOne({
-    _id: req.params.id,
-  })
-    .then((thing) => {
-      res.status(200).json(thing);
+    Thing.findOne({
+        _id: req.params.id,
     })
-    .catch((error) => {
-      res.status(404).json({
-        error: error,
-      });
-    });
+        .then((thing) => {
+            res.status(200).json(thing);
+        })
+        .catch((error) => {
+            res.status(404).json({
+                error: error,
+            });
+        });
 };
 
 exports.modifyThing = (req, res, next) => {
-  const thing = new Thing({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId,
-  });
-  Thing.updateOne({ _id: req.params.id }, thing)
-    .then(() => {
-      res.status(201).json({
-        message: "Thing updated successfully!",
-      });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
-    });
+    const thingObject = req.file
+        ? {
+              ...JSON.parse(req.body.thing),
+              imageUrl: `${req.protocol}://${req.get("host")}/images/${
+                  req.file.filename
+              }`,
+          }
+        : { ...req.body };
+    Thing.updateOne(
+        { _id: req.params.id },
+        { ...thingObject, _id: req.params.id }
+    )
+        .then(() => res.status(200).json({ message: "Objet modifié !" }))
+        .catch((error) => res.status(400).json({ error }));
 };
 
 exports.deleteThing = (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => {
-      res.status(200).json({
-        message: "Deleted!",
-      });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
-    });
+    Thing.findOne({ _id: req.params.id })
+        .then((thing) => {
+            const filename = thing.imageUrl.split("/images/")[1];
+            fs.unlink(`images/${filename}`, () => {
+                Thing.deleteOne({ _id: req.params.id })
+                    .then(() =>
+                        res.status(200).json({ message: "Objet supprimé !" })
+                    )
+                    .catch((error) => res.status(400).json({ error }));
+            });
+        })
+        .catch((error) => res.status(500).json({ error }));
 };
 
 exports.getAllStuff = (req, res, next) => {
-  Thing.find()
-    .then((things) => {
-      res.status(200).json(things);
-    })
-    .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
-    });
+    Thing.find()
+        .then((things) => {
+            res.status(200).json(things);
+        })
+        .catch((error) => {
+            res.status(400).json({
+                error: error,
+            });
+        });
 };
